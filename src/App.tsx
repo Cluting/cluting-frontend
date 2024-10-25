ab3f7cb9317abb4838db8a9e92d7a99a383e4270
yeonilil <gwagseoyeon@gmail.com>
2024-10-25 17:56:08 +0900

    ✨ feat: Footer 구현

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,19 @@ import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <div className="bg-bg-100 h-screen w-full pt-[55px]">
+      <div className="relative bg-gray-100 h-screen w-full pt-[55px]">
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/FloatingButton.tsx
@@ -0,0 +1,5 @@
+export default function FloatingButton() {
+  return (
+    <div className="absolute bottom-0 bg-gray-200 w-full h-[130px]">Footer</div>
+  );
+}
