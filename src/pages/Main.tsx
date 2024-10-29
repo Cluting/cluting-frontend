a2ab43cf83377dc0aba9b91aea5fcff556e9f084
yxxndxxn <yxxndxxn@gmail.com>
2024-10-29 15:54:46 +0900

    🎨style: Main에 카드 컴포넌트 배치

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,23 @@
+import ClubCard from "../components/recruting/home/ClubCard";
+
 export default function Main() {
-  return <div className="text-[30px]">메인 페이지 입니다</div>;
+  return (
+    <div className="container max-w-[1072px] mx-auto px-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[53px]">
+        <ClubCard />
+        <ClubCard />
+        <ClubCard />
+        <ClubCard />
+        <ClubCard />
+        <ClubCard />
+        <ClubCard />
+        <ClubCard />
+        <ClubCard />
+        <ClubCard />
+        <ClubCard />
+        <ClubCard />
+      </div>
+    </div>
+  );
+  // return <div className="text-[30px]">메인 페이지 입니다</div>;
 }
