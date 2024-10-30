5395406a111219085a6cc3f51eff6b763e91e946
yeonilil <gwagseoyeon@gmail.com>
2024-10-30 12:18:37 +0900

    🎨 style: 리크루팅 홈 상단 RecrutingStep UI

--- src/components/recruting/RecruitmentStep.tsx
+++ src/components/recruting/RecruitmentStep.tsx
@@ -1,5 +1,81 @@
 //리크루팅 단계
 
 export default function RecruitmentStep() {
-  return <div className="text-[30px] "></div>;
+  return (
+    <div className="flex">
+      <div className="flex flex-col items-center">
+        <section>
+          <div className="bg-gray-100 px-[19px] py-[8px] rounded-[7px] text-left">
+            <p className="text-caption1 text-gray-700">Today</p>
+            <p className="text-gray-900 text-title3">10월 13일 화요일</p>
+          </div>
+        </section>
+
+        <section className="mt-5 mb-[25px]">
+          <p className="text-subheadline">{"리크루팅을 시작해 주세요."}</p>
+        </section>
+      </div>
+
+      <section className="flex items-center ml-10">
+        <div>
+          <div className="w-[70px] h-[70px] rounded-full bg-gray-400"></div>
+          <p className="text-caption2 text-gray-700 mt-2">계획 세우기</p>
+        </div>
+        <img
+          src="/assets/ic-progress.svg"
+          alt="진행 단계"
+          className=" w-[46px] h-[6px] mx-4 mb-5"
+        />
+
+        <div>
+          <div className="w-[70px] h-[70px] rounded-full bg-gray-400"></div>
+          <p className="text-caption2 text-gray-700 mt-2">모집 준비하기</p>
+        </div>
+        <img
+          src="/assets/ic-progress.svg"
+          alt="진행 단계"
+          className=" w-[46px] h-[6px] mx-4 mb-5"
+        />
+
+        <div>
+          <div className="w-[70px] h-[70px] rounded-full bg-gray-400"></div>
+          <p className="text-caption2 text-gray-700 mt-2">서류 평가하기</p>
+        </div>
+        <img
+          src="/assets/ic-progress.svg"
+          alt="진행 단계"
+          className=" w-[46px] h-[6px] mx-4 mb-5"
+        />
+
+        <div>
+          <div className="w-[70px] h-[70px] rounded-full bg-gray-400"></div>
+          <p className="text-caption2 text-gray-700 mt-2">
+            서류 합격자 및 <br /> 면접 안내
+          </p>
+        </div>
+        <img
+          src="/assets/ic-progress.svg"
+          alt="진행 단계"
+          className=" w-[46px] h-[6px] mx-4 mb-5"
+        />
+
+        <div>
+          <div className="w-[70px] h-[70px] rounded-full bg-gray-400"></div>
+          <p className="text-caption2 text-gray-700 mt-2">면접 평가하기</p>
+        </div>
+        <img
+          src="/assets/ic-progress.svg"
+          alt="진행 단계"
+          className=" w-[46px] h-[6px] mx-4 mb-5"
+        />
+
+        <div>
+          <div className="w-[70px] h-[70px] rounded-full bg-gray-400"></div>
+          <p className="text-caption2 text-gray-700 mt-2">
+            최종 합격자 및 <br /> 활동 안내
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 }
--- src/pages/Recruting/RecrutingHome.tsx
+++ src/pages/Recruting/RecrutingHome.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import RecrutingStartModal from "../../components/recruting/home/RecrutingStartModal";
 import Sidemenu from "../../components/recruting/Sidemenu";
 import TodoTemplate from "../../components/recruting/home/TodoTemplate";
+import RecruitmentStep from "../../components/recruting/RecruitmentStep";
 
 export default function RecrutingHome() {
   const [isModalOpen, setIsModalOpen] = useState(false); // 모달 가시성 상태
@@ -21,13 +22,14 @@ export default function RecrutingHome() {
       {" "}
       <Sidemenu />
       <div className="flex flex-col">
-        <div className="w-[1000px] h-auto bg-white-100 py-6 mx-8 px-[13px] rounded-[12px]">
+        <div className="w-[1100px] h-auto bg-white-100 py-6 mx-8 px-[13px] rounded-[12px]">
+          <RecruitmentStep />
           <section className="w-min">
             <p className="text-headline mb-[30px] text-left">개인 TODO</p>
             <TodoTemplate />
           </section>
         </div>
-        <section className="w-[1000px] bg-gray-400 text-headline flex items-center justify-between mx-8 mt-[14px] py-[15px] px-[30px] rounded-[12px]">
+        <section className="w-[1100px] bg-gray-400 text-headline flex items-center justify-between mx-8 mt-[14px] py-[15px] px-[30px] rounded-[12px]">
           <p className="mt-7 text-headline text-gray-800 mb-[30px] text-left">
             아직 리크루팅을 시작하지 않았어요. 리크루팅을 시작해 주세요!
           </p>
