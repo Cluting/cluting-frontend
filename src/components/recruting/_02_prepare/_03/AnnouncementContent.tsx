2bf5e718da1c7d5244d032252b1fa5353f385624
yeonilil <gwagseoyeon@gmail.com>
2024-11-01 16:30:56 +0900

    ✨ feat: 글자수 세기 추가

--- src/components/recruting/_02_prepare/_03/AnnouncementContent.tsx
+++ src/components/recruting/_02_prepare/_03/AnnouncementContent.tsx
@@ -1,25 +1,33 @@
-import React, { useRef, FormEvent } from "react";
+import React, { useRef, FormEvent, useState } from "react";
+const MAX_CHARS = 2000;
 
 const AnnouncementContent: React.FC = () => {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const [charCount, setCharCount] = useState(0);
 
   const handleInput = (event: FormEvent<HTMLTextAreaElement>): void => {
     const textarea = textareaRef.current;
     if (textarea) {
+      setCharCount(textarea.value.length);
       textarea.style.height = "290px"; // 기본 높이 설정
       textarea.style.height = `${textarea.scrollHeight}px`; // 내용에 따라 높이 조정
     }
   };
   //FIX: onInput 리렌더링 문제 고민해보기
   return (
-    <textarea
-      ref={textareaRef}
-      placeholder="상세 본문 내용을 작성해 주세요"
-      maxLength={2000}
-      onInput={handleInput}
-      className="input-background w-full min-h-[290px] mx-8 px-[26px] py-[22px] rounded-[12px] mb-[50px] overflow-hidden"
-      style={{ resize: "none" }} // 수동 크기 조절 비활성화
-    />
+    <div className="relative">
+      <textarea
+        ref={textareaRef}
+        placeholder="상세 본문 내용을 작성해 주세요"
+        maxLength={2000}
+        onInput={handleInput}
+        className="input-background w-full min-h-[290px] mx-8 px-[26px] py-[22px] rounded-[12px] mb-[50px] overflow-hidden"
+        style={{ resize: "none" }} // 수동 크기 조절 비활성화
+      />
+      <div className="text-right text-gray-500 absolute right-0 bottom-[80px]">
+        {charCount}/{MAX_CHARS}
+      </div>
+    </div>
   );
 };
 
