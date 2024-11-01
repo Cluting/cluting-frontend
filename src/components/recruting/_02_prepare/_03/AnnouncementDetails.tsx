2662dcb23be2af0a3532c6a217981fa4c99474c3
yeonilil <gwagseoyeon@gmail.com>
2024-11-01 15:54:08 +0900

    ✨ feat: 포스터 업로드 기능 추가

--- src/components/recruting/_02_prepare/_03/AnnouncementDetails.tsx
+++ src/components/recruting/_02_prepare/_03/AnnouncementDetails.tsx
@@ -1,10 +1,54 @@
 //2-3 공고 작성
 
+import { useRef, useState } from "react";
+
 export default function AnnouncementDetails() {
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    fileInputRef.current?.click(); // 클릭 시 파일 선택 창 열기
+  };
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const fileList = event.target.files;
+    if (fileList) {
+      const file = fileList[0];
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreviewUrl(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <form className="flex flex-col bg-white-100 py-6 mx-8 mb-9 px-10 rounded-[12px] w-full text-left">
       <label className="mt-6">포스터 업로드</label>
-      <input type="file" className="input-background input-style" />
+      <button
+        type="button"
+        onClick={handleClick}
+        className="h-60 input-background input-style border-dashed"
+      >
+        {previewUrl ? (
+          <img
+            src={previewUrl}
+            alt="포스터 사진"
+            className="w-full h-full object-cover rounded-full"
+          />
+        ) : (
+          <div>
+            <input
+              ref={fileInputRef}
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              style={{ display: "none" }}
+            />
+            사진 불러오기
+          </div>
+        )}
+      </button>
 
       <label className="mt-6">공고 제목</label>
       <input type="text" className="input-background input-style" />
