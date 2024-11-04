a5cf0a84c7ae17b0e79ef1d6b9c34ffdd6b1bcab
yeonilil <gwagseoyeon@gmail.com>
2024-11-04 19:11:56 +0900

    ✨ feat: 면접 형식 설정하기 컴포넌트 추가

--- src/components/recruting/_02_prepare/_04/InterviewFormat.tsx
+++ src/components/recruting/_02_prepare/_04/InterviewFormat.tsx
@@ -1,4 +1,32 @@
 // 1 - 면접 형식 설정하기
 export default function InterviewFormat() {
-  return <div className="section-background "></div>;
+  return (
+    <div className="section-background ">
+      <div className="flex items-center">
+        <div className="flex flex-col">
+          <p className="text-gray-850 text-callout">면접관</p>
+          <div className="flex items-center">
+            <input
+              type="number"
+              min={0}
+              className="w-20 input-style input-background mr-[11px]"
+            />
+            <p className="text-headline">명</p>
+          </div>
+        </div>
+        <p className="mx-5 text-headline text-gray-800 mt-5">:</p>
+        <div className="flex flex-col">
+          <p className="text-gray-850 text-callout">면접자</p>
+          <div className="flex items-center">
+            <input
+              type="number"
+              min={0}
+              className="w-20 input-style input-background mr-[11px]"
+            />
+            <p className="text-headline">명</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
