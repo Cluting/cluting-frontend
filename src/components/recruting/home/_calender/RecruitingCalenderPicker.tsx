52c903a2519c9ea141ff9a0cbe37d99b6dbc4beb
yeonilil <gwagseoyeon@gmail.com>
2024-11-08 09:40:49 +0900

    ✨ feat: 면접 일정 완료 시 취소선 추가, 면접 일정 한 개로 제한

--- src/components/recruting/home/_calender/RecruitingCalenderPicker.tsx
+++ src/components/recruting/home/_calender/RecruitingCalenderPicker.tsx
@@ -13,6 +13,13 @@ interface CalendarEvent {
   allDay: boolean;
 }
 
+const buttonItems = [
+  "지원자 리스트 보기",
+  "면접 일정 바로가기",
+  "서류 합격자 리스트 보기",
+  "최종 합격 리스트 보기"
+];
+
 export default function RecrutingCalenderPicker() {
   const [events, setEvents] = useState<CalendarEvent[]>([]);
   const [currrentTitle, setCurrentTitle] = useState("");
@@ -20,6 +27,10 @@ export default function RecrutingCalenderPicker() {
     start: string;
     end: string;
   } | null>(null); // 선택된 날짜 범위 상태
+  const [instructionMessage, setInstructionMessage] = useState<string | null>(
+    null
+  ); // 드래그 시작시 표시되는 안내 메시지
+  const [completedTitles, setCompletedTitles] = useState<string[]>([]); // 완료된 제목 상태
 
   // 날짜 선택 시 이벤트 추가 함수
   const handleDateSelect = (selectInfo: DateSelectArg) => {
@@ -28,7 +39,10 @@ export default function RecrutingCalenderPicker() {
 
     calendarApi.unselect(); // 선택 해제
 
-    if (title) {
+    // 이미 존재하는 제목인지 확인
+    const isDuplicateTitle = events.some((event) => event.title === title);
+
+    if (title && !isDuplicateTitle) {
       const newEvent: CalendarEvent = {
         id: String(events.length + 1),
         title,
@@ -37,7 +51,17 @@ export default function RecrutingCalenderPicker() {
         allDay: selectInfo.allDay
       };
       setEvents((prevEvents) => [...prevEvents, newEvent]);
+      setCompletedTitles((prevTitles) => [...prevTitles, title]); // 완료된 제목에 추가
+      console.log(events);
+      setInstructionMessage("");
+    } else if (isDuplicateTitle) {
+      alert("같은 제목의 이벤트가 이미 있습니다.");
     }
+
+    // 날짜 선택 시작 시 안내 메시지 표시
+    setInstructionMessage(
+      "캘린더 위로 해당 일정의 시작 날짜부터 마지막 날짜까지 드래그 해주세요."
+    );
   };
 
   // 이벤트 클릭 시 삭제 함수
@@ -47,68 +71,45 @@ export default function RecrutingCalenderPicker() {
       setEvents((prevEvents) =>
         prevEvents.filter((event) => event.id !== clickInfo.event.id)
       );
+      setCompletedTitles((prevTitles) =>
+        prevTitles.filter((title) => title !== clickInfo.event.title)
+      ); // 취소된 제목은 완료 리스트에서 제거
     }
   };
 
   return (
     <div className="mt-[30px] mx-10 bg-white-100">
       <div className="flex gap-10 text-left">
-        <section className="w-[300px] text-caption3 flex flex-col">
-          <button
-            className="flex items-center bg-white-100 py-3 pl-4 my-[6px] rounded-[8px] border border-gray-200 text-subheadline text-gray-900"
-            onClick={() => {
-              setCurrentTitle("지원자 리스트 보기");
-            }}
-          >
-            <img
-              src="/assets/ic-dateList.svg"
-              alt="리크루팅 일정 리스트"
-              className="w-[17px] h-[20px] mr-2"
-            />
-            지원자 리스트 보기
-          </button>
-          <button
-            onClick={() => {
-              setCurrentTitle("면접 일정 바로가기");
-            }}
-            className="flex items-center bg-white-100 py-3 pl-4 my-[6px] rounded-[8px] border border-gray-200 text-subheadline text-gray-900"
-          >
-            <img
-              src="/assets/ic-dateList.svg"
-              alt="리크루팅 일정 리스트"
-              className="w-[17px] h-[20px] mr-2"
-            />
-            면접 일정 바로가기
-          </button>
-          <button
-            onClick={() => {
-              setCurrentTitle("서류 합격자 리스트 보기");
-            }}
-            className="flex items-center bg-white-100 py-3 pl-4 my-[6px] rounded-[8px] border border-gray-200 text-subheadline text-gray-900"
-          >
-            <img
-              src="/assets/ic-dateList.svg"
-              alt="리크루팅 일정 리스트"
-              className="w-[17px] h-[20px] mr-2"
-            />
-            서류 합격자 리스트 보기
-          </button>
-          <button
-            onClick={() => {
-              setCurrentTitle("최종 합격 리스트 보기");
-            }}
-            className="flex items-center bg-white-100 py-3 pl-4 my-[6px] rounded-[8px] border border-gray-200 text-subheadline text-gray-900"
-          >
-            <img
-              src="/assets/ic-dateList.svg"
-              alt="리크루팅 일정 리스트"
-              className="w-[17px] h-[20px] mr-2"
-            />
-            최종 합격 리스트 보기
-          </button>
+        <section className="w-[300px] text-caption3 bg-gray-100 p-[15px] border border-gray-400 flex flex-col rounded-[12px]">
+          {buttonItems.map((item) => (
+            <button
+              key={item}
+              className={`${
+                currrentTitle === item
+                  ? "bg-gray-200 border border-gray-400"
+                  : "bg-white-100 border border-gray-200"
+              } flex items-center py-3 pl-4 my-[6px] rounded-[8px] text-subheadline text-gray-900 ${
+                completedTitles.includes(item) ? "line-through" : ""
+              }`}
+              onClick={() => setCurrentTitle(item)}
+            >
+              <img
+                src="/assets/ic-dateList.svg"
+                alt="리크루팅 일정 리스트"
+                className="w-[17px] h-[20px] mr-2"
+              />
+              {item}
+            </button>
+          ))}
         </section>
 
-        <div className="datepicker-container relative">
+        <div className="flex flex-col datepicker-container relative">
+          {/* 안내 메시지 표시 */}
+          {instructionMessage && (
+            <div className="text-red-100 text-caption1 mb-2 rounded">
+              {instructionMessage}
+            </div>
+          )}
           <FullCalendar
             locale="ko"
             plugins={[dayGridPlugin, interactionPlugin]}
