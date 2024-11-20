5a25e4c4d74f83292028fd4481f0bd73b36c29d6
yeonilil <gwagseoyeon@gmail.com>
2024-11-20 17:57:31 +0900

    ✨ feat: 운영진 평가 섹션 구현

--- src/pages/Recruting/step3/Evalutation.tsx
+++ src/pages/Recruting/step3/Evalutation.tsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import ApplicationQuestion from "../../../components/recruting/_03_document_evaluation/evaluation/ApplicationQuestion";
 import UserProfile from "../../../components/recruting/_03_document_evaluation/evaluation/UserProfile";
 import Portfolio from "../../../components/recruting/_03_document_evaluation/evaluation/Portfolio";
+import AdminEvaluationWindow from "../../../components/recruting/_03_document_evaluation/evaluation/AdminEvaluationWindow";
 
 //3 - 리크루팅 : 서류 평가하기 단계
 export default function Evaluation() {
   const [view, setView] = useState("application"); // 초기 상태는 "application"
   return (
-    <div className="flex flex-col items-center h-full pt-6 bg-gray-100 ">
-      <div className="w-[829px] mb-[84px] h-full">
+    <div className=" flex flex-col items-center h-full pt-6 bg-gray-100 ">
+      <div className="relative w-[829px] mb-[84px] h-full ">
         <div className="w-full flex justify-end">
           <button
             onClick={() => setView("application")}
@@ -35,6 +36,8 @@ export default function Evaluation() {
             <Portfolio />
           </>
         )}
+
+        <AdminEvaluationWindow />
       </div>
     </div>
   );
