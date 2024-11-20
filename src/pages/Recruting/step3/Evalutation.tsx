c0b5b5905d255bc9f5d3a832629a3858086c1529
yeonilil <gwagseoyeon@gmail.com>
2024-11-20 15:14:22 +0900

    ✨ feat: 지원서류, 포트폴리오 선택 버튼 구현

--- src/pages/Recruting/step3/Evalutation.tsx
+++ src/pages/Recruting/step3/Evalutation.tsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import ApplicationQuestion from "../../../components/recruting/_03_document_evaluation/evaluation/ApplicationQuestion";
 import UserProfile from "../../../components/recruting/_03_document_evaluation/evaluation/UserProfile";
 
 //3 - 리크루팅 : 서류 평가하기 단계
 export default function Evaluation() {
+  const [view, setView] = useState("application"); // 초기 상태는 "application"
   return (
     <div className="flex flex-col items-center h-full pt-6 bg-gray-100 ">
-      <div className="w-[829px] mb-[84px]">
-        <UserProfile />
-        <ApplicationQuestion />
+      <div className="w-[829px] mb-[84px] h-full">
+        <div className="w-full flex justify-end">
+          <button
+            onClick={() => setView("application")}
+            className="button-main-bg-sm "
+          >
+            지원서류
+          </button>
+          <button
+            onClick={() => setView("portfolio")}
+            className="ml-4 button-main-light-sm"
+          >
+            포트폴리오
+          </button>
+        </div>
+        {view === "application" && (
+          <>
+            <UserProfile />
+            <ApplicationQuestion />
+          </>
+        )}
       </div>
     </div>
   );
