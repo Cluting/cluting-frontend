152e94222a6203f1c31d166db8b51277cad3c116
yeonilil <gwagseoyeon@gmail.com>
2024-11-25 13:15:24 +0900

    🎨 style: 4-1 변경된 디자인 반영

--- src/components/recruting/_04_interview_notification/_01/ApplicantStatusContainer.tsx
+++ src/components/recruting/_04_interview_notification/_01/ApplicantStatusContainer.tsx
@@ -13,20 +13,18 @@ export default function ApplicantStatusContainer() {
       <div className="w-full flex ">
         <div className="w-full flex justify-between items-center">
           <div className="flex items-center">
-            <div className="bg-white-100 border border-gray-200 rounded-[6px] mr-[21px] py-[7.5px] px-[11px] text-[14px]">
+            <div className="flex-center gap-[17px] bg-white-100 border border-gray-200 rounded-[6px] mr-[21px] py-[6px] px-[11px] text-[14px]">
               <span className="font-semibold">필터:</span> {filter}
+              <img src="/assets/ic-dropdown.svg" />
             </div>
-            <div className="bg-white-100 border border-gray-200 rounded-[6px] py-[7.5px] px-[11px] text-[14px]">
+            <div className="flex-center gap-[17px] bg-white-100 border border-gray-200 rounded-[6px] py-[6px] px-[11px] text-[14px]">
               <span className="font-semibold">정렬:</span> 가나다순
+              <img src="/assets/ic-dropdown.svg" />
             </div>
-          </div>
-
-          <div className="tooltip">
-            마지막으로 최종 합불 결과를 확인해 주세요.
           </div>
         </div>
       </div>
-      <div className="bg-gray-50 w-full mt-[18px] px-[25px] py-[18px] border border-gray-200 rounded-[6.65px] ">
+      <div className="bg-gray-50 w-full mt-[18px] px-[25px] py-[18px] border border-gray-200 rounded-[21px] ">
         <div className="grid grid-cols-2">
           <PassList filter={filter} />
           <FailList filter={filter} />
--- src/components/recruting/_04_interview_notification/_01/FailList.tsx
+++ src/components/recruting/_04_interview_notification/_01/FailList.tsx
@@ -20,34 +20,34 @@ export default function FailList({ filter }: failListProps) {
     <div className="w-max-[440px] pl-[10px]">
       <div className="flex items-center gap-2">
         <div className="flex items-center">
-          <div className="text-[#525252] text-[13.3px] mr-[5px] p-[6px] rounded-[5px] bg-white-100 border border-gray-200">
+          <div className="text-[#525252] text-[13.3px] mr-[5px] p-[6px] rounded-[5px] bg-gray-100 ">
             불합격자
           </div>
           <p className="text-[#6F7278] text-[11px]">{"78"}</p>
         </div>
       </div>
       <table className="w-full mt-4">
-        <thead className="w-full bg-[#F4F4F4] border border-[#D6D7DA]  rounded-[6.42px] text-[12.75px] text-[#7E7E7E]">
+        <thead className="w-full bg-gray-100  rounded-[6.42px] text-[12.75px] text-gray-800">
           <tr>
-            <th className="py-[11px] w-[57px]">상태</th>
+            <th className="py-[11px] w-[80px]">상태</th>
             <th className="py-[11px] w-[100px]">이름</th>
-            <th className="py-[11px]">구분</th>
+            <th className="py-[11px]">그룹</th>
             <th className="py-[11px]">순위</th>
             <th className="py-[11px]">결과</th>
           </tr>
         </thead>
         <tbody>
           {filteredData.map((applicant) => (
-            <tr className="h-[50px] text-[12.85px] font-semibold border-b border-[#D6D7DA]">
+            <tr className="h-[50px] text-[12.85px] text-gray-1100 font-semibold border-b border-[#D6D7DA]">
               <td>
-                <div className="bg-[#9EA2A9] rounded-[4.82px] text-white-100 flex-center py-[5px] px-[5px] text-[11px] font-semibold">
-                  {applicant.status}
+                <div className="bg-gray-100 rounded-[38px] text-gray-500 flex-center py-[5px] px-[5px] text-caption3">
+                  {applicant.status === "완료" ? "결정 완료" : ""}
                 </div>
               </td>
               <td>
                 <div className="flex flex-col items-start ml-[25px]">
                   <div className="text-3"> {applicant.name}</div>
-                  <div className="font-normal text-[9.64px] text-gray-800">
+                  <div className="font-normal text-[9.64px] text-gray-500">
                     {applicant.phone}
                   </div>
                 </div>
