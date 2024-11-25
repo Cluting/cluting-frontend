db53f82379c8c147fc50a395d690c6e581938dd1
yeonilil <gwagseoyeon@gmail.com>
2024-11-25 13:50:02 +0900

    ✨ feat: 필터 드롭다운 추가

---
+++ src/components/recruting/_04_interview_notification/_01/AlignDropdown.tsx
@@ -0,0 +1,18 @@
+interface alignDropdownProps {
+  align: string;
+}
+
+export default function AlignDropdown({ align }: alignDropdownProps) {
+  return (
+    <div className="animate-dropdown absolute top-[40px] left-0 z-50 bg-white-100 w-[130px] py-2 rounded-[12px] border border-gray-300">
+      <ul className="flex flex-col items-center justify-center h-full">
+        <li className="small-dropdown-list">
+          <p className="text-center text-gray-800 font-medium">지원순</p>
+        </li>
+        <li className="small-dropdown-list">
+          <p className="text-center text-gray-800 font-medium">최신순</p>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- src/components/recruting/_04_interview_notification/_01/ApplicantStatusContainer.tsx
+++ src/components/recruting/_04_interview_notification/_01/ApplicantStatusContainer.tsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import FailList from "./FailList";
 import PassList from "./PassList";
+import AlignDropdown from "./AlignDropdown";
+import FilterDropdown from "./FilterDropdown";
 
 // 4-1 지원자 합불 결과 (컨테이너)
 export default function ApplicantStatusContainer() {
-  //FIX: 필터 지정할 수 있는 드롭다운 구현 - 우선 현재는 기획으로 필터링
-  const [filter, setFilter] = useState("기획");
+  const [filter, setFilter] = useState("전체");
+  const [align, setAlign] = useState("지원순");
+
+  const [showFilter, setShowFilter] = useState(false);
+  const [showAlign, setShowAlign] = useState(false);
 
   //TODO: 정렬은 백에서 처리한 데이터로
   return (
@@ -13,13 +18,32 @@ export default function ApplicantStatusContainer() {
       <div className="w-full flex ">
         <div className="w-full flex justify-between items-center">
           <div className="flex items-center">
-            <div className="flex-center gap-[17px] bg-white-100 border border-gray-200 rounded-[6px] mr-[21px] py-[6px] px-[11px] text-[14px]">
+            <div
+              onClick={() => {
+                setShowFilter(!showFilter);
+              }}
+              className="relative flex-center gap-[17px] bg-white-100 border border-gray-200 rounded-[6px] mr-[21px] py-[6px] px-[11px] text-[14px]"
+            >
               <span className="font-semibold">필터:</span> {filter}
               <img src="/assets/ic-dropdown.svg" />
+              {showFilter && (
+                <FilterDropdown
+                  onSelectFilter={(selectedFilter) => {
+                    setFilter(selectedFilter); // 필터 업데이트
+                    setShowFilter(false); // 드롭다운 닫기
+                  }}
+                />
+              )}
             </div>
-            <div className="flex-center gap-[17px] bg-white-100 border border-gray-200 rounded-[6px] py-[6px] px-[11px] text-[14px]">
+            <div
+              onClick={() => {
+                setShowAlign(!showAlign);
+              }}
+              className="relative flex-center gap-[17px] bg-white-100 border border-gray-200 rounded-[6px] py-[6px] px-[11px] text-[14px]"
+            >
               <span className="font-semibold">정렬:</span> 가나다순
               <img src="/assets/ic-dropdown.svg" />
+              {showAlign && <AlignDropdown align={align} />}
             </div>
           </div>
         </div>
