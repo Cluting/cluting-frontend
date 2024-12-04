13cb65bfc1e83c6df8809947697b7d6cb931ec25
yeonilil <gwagseoyeon@gmail.com>
2024-12-04 17:23:16 +0900

    ♻️ refactor: admin 드롭다운 수정

--- src/components/recruting/_03_document_evaluation/_01/AddAdminDropdown.tsx
+++ src/components/recruting/_03_document_evaluation/_01/AddAdminDropdown.tsx
@@ -1,8 +1,8 @@
 import { ALL_ADMINS } from "../../../../constants/recruting";
 
 interface AddAdminDropdownProps {
-  onSelect: (admin: string) => void; // 운영진 선택시 호출될 함수
-  currentAdmins: string[]; // 현재 선택된 운영진 목록
+  onSelect: (adminId: string) => void; // 운영진 선택시 호출될 함수
+  currentAdmins: Admin[]; // 현재 선택된 운영진 목록
 }
 
 export default function AddAdminDropdown({
@@ -12,14 +12,16 @@ export default function AddAdminDropdown({
   return (
     <div className="absolute animate-dropdown top-[53.5px] w-[328px] bg-white-100 rounded-[12px] z-50">
       <ul className="flex-center flex-col h-full p-2 text-body text-gray-1100">
-        {ALL_ADMINS.map((admin: string) => (
+        {ALL_ADMINS.map((admin) => (
           <li
-            key={admin}
-            onClick={() => onSelect(admin)}
+            key={admin.id}
+            onClick={() => onSelect(admin.id)}
             className="relative w-full h-[40px] rounded-[8px] cursor-pointer hover:bg-gray-200 flex-center z-50"
           >
-            <span className="text-gray-1100 font-medium">{admin}</span>
-            {currentAdmins.includes(admin) && (
+            <span className="text-gray-1100 font-medium">{admin.name}</span>
+            {currentAdmins.some(
+              (currentAdmin) => currentAdmin.id === admin.id
+            ) && (
               <span className="absolute text-main-100 ml-1 left-[calc(50%+2rem)]">
                 ✓
               </span>
