29e607390e314567fd601e29ea5a99564b22a709
gracekim527 <lemonherb0323@naver.com>
2024-12-11 20:50:33 +0900

    :bulb: Chore: 최상위 부모 컴포넌트 코멘트 추가 (#94)

--- src/components/recruting/_05_interview_evaluation/_01/PrepareInterviewEvaluationContainer.tsx
+++ src/components/recruting/_05_interview_evaluation/_01/PrepareInterviewEvaluationContainer.tsx
@@ -8,9 +8,13 @@ export default function PrepareInterviewEvaluationContainer() {
   return (
     // Conflict :: 다인님 부분과 컨플릭트 날 예정
     <div className="flex flex-col gap-12">
+      {/* 면접 질문 개수 설정하기 */}
       <InterviewQuestionCountSection />
+      {/* 공통 면접 질문 만들기 */}
       <CommonInterviewQuestionsSection />
+      {/* 그룹별 면접 질문 만들기 */}
       <GroupInterviewQuestionsSection />
+      {/* 개별 면접 질문 만들기 */}
       <IndividualInterviewQuestionsSection />
     </div>
   );
