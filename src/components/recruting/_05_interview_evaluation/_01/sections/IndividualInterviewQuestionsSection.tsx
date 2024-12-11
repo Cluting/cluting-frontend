ebbb00d97d05af16ae3fb9337d23b411eee7afe2
gracekim527 <lemonherb0323@naver.com>
2024-12-11 20:37:09 +0900

    :sparkles: Feat: 개별 면접 질문 프로세스바 구현 (#94)

--- src/components/recruting/_05_interview_evaluation/_01/sections/IndividualInterviewQuestionsSection.tsx
+++ src/components/recruting/_05_interview_evaluation/_01/sections/IndividualInterviewQuestionsSection.tsx
@@ -1,9 +1,28 @@
 import { useState } from "react";
 import Dropdown from "./dropdown/Dropdown";
 import Headline from "./headline/Headline";
+import ProgressBar from "./processBar/ProcessBar";
 
 export default function IndividualInterviewQuestionsSection() {
   const [filter, setFilter] = useState("전체");
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const steps = ["작성 전", "작성 중", "작성 완료"];
+
+  const renderStepComponents = () => {
+    switch (currentStep) {
+      case 0:
+        return null;
+      case 1:
+        return null;
+      case 2:
+        return null;
+
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="flex flex-col gap-2">
       <Headline
@@ -15,12 +34,19 @@ export default function IndividualInterviewQuestionsSection() {
         <button>권한자 보기</button>
       </Headline>
       <div className="flex flex-col gap-2 p-4 bg-white-100 border border-[#D9D4E7] rounded-2xl">
-        <Dropdown
-          label="필터 : "
-          defaultValue="전체"
-          options={["전체", "기획", "디자인", "개발"]}
-          onSelect={(value) => setFilter(value)}
-        />
+        <div className="flex flex-col gap-3">
+          <Dropdown
+            label="필터 : "
+            defaultValue="전체"
+            options={["전체", "기획", "디자인", "개발"]}
+            onSelect={(value) => setFilter(value)}
+          />
+          <ProgressBar
+            steps={steps}
+            currentStep={currentStep}
+            onStepClick={(stepIndex) => setCurrentStep(stepIndex)}
+          />
+        </div>
       </div>
     </div>
   );
