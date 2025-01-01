9ff99b5a421b942eb1abbfbe148fa5a9f76a1c16
yeonilil <gwagseoyeon@gmail.com>
2025-01-01 22:08:52 +0900

    ♻️ refactor: 2-4 면접 형식 등록 form 설정

---
+++ src/components/recruting/_02_prepare/_04/type/2-4.d.ts
@@ -0,0 +1,6 @@
+// 모집하기(4) - 운영진 면접 일정 조정하기-면접세팅 API 제출
+declare interface InterviewSetup {
+  interviewer: number;
+  interviewee: number;
+  interviewDuration: number;
+}
--- src/components/recruting/_02_prepare/service/Step2.ts
+++ src/components/recruting/_02_prepare/service/Step2.ts
@@ -10,3 +10,20 @@ export async function postPrepare3(recruitId: number, data: AnnouncementForm) {
     throw error;
   }
 }
+
+// POST: 모집하기(4) - 운영진 면접 일정 조정하기-면접세팅
+export async function postPrepare4InterviewSetup(
+  recruitId: number,
+  data: InterviewSetup
+) {
+  try {
+    const response = await Instance.post(
+      `/plan/stage4/${recruitId}/interview-setup`,
+      data
+    );
+    console.log("성공:", response.data);
+  } catch (error) {
+    console.error("모집하기(4) 운영진 면접 일정 조정 POST 실패:", error);
+    throw error;
+  }
+}
