c60b0ba87486868f6a7e1283a0acd215fd679098
yxxndxxn <yxxndxxn@gmail.com>
2025-01-17 00:16:44 +0900

    ✨ feat: 공고보기 컴포넌트 분리

--- src/components/applicant/application_history/detail/container/RecrutingInfoContainer/RecrutingInfoContainer.tsx
+++ src/components/applicant/application_history/detail/container/RecrutingInfoContainer/RecrutingInfoContainer.tsx
@@ -4,7 +4,7 @@ import { GradeDistributionSection } from "./GradeDistributionSection";
 
 export default function RecrutingInfoContainer() {
   return (
-    <div className="relative w-full min-h-[1062px] px-7 py-[26px]">
+    <div className="relative flex w-full min-h-[1062px] px-7 py-[26px]">
       <article className="absolute right-7 w-[390px] px-[19px] py-[17px] bg-gray-50 border border-gray-200 rounded-lg">
         <ScoreSection />
         <CompetitionSection />
