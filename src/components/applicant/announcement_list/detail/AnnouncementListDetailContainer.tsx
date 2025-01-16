266edea6ddc829c474ed2483596cdb92c995dc79
yxxndxxn <yxxndxxn@gmail.com>
2025-01-16 19:47:05 +0900

    ✨ feat: Link 연결

--- src/components/applicant/announcement_list/detail/AnnouncementListDetailContainer.tsx
+++ src/components/applicant/announcement_list/detail/AnnouncementListDetailContainer.tsx
@@ -2,7 +2,10 @@ import { useLocation, useNavigate } from "react-router-dom";
 import AnnouncementContainer from "./container/AnnouncementContainer";
 import DocumentSubmitContainer from "./container/DocumentSubmitContainer";
 import InqueryContainer from "./container/InqueryContainer";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
 // 공고 클릭 시 상세
+
 export default function AnnouncementListDetailContainer() {
   const navigate = useNavigate();
   const location = useLocation();
@@ -14,9 +17,25 @@ export default function AnnouncementListDetailContainer() {
     navigate(`${location.pathname}?${queryParams.toString()}`);
   };
 
+  // 컴포넌트 마운트 시 스크롤을 최상단으로 이동
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="w-full h-auto bg-gray-100">
       <div className="flex flex-col items-start ">
+        <header className="mb-[39px] flex-center gap-5 ">
+          <Link to="/main">
+            <img src="/assets/ic-back.svg" alt="뒤로가기" />
+          </Link>
+          <h1 className="font-semibold text-[24px]">
+            대학생 연합 IT 동아리 잇타 7기 신입 회원 모집
+          </h1>
+          <div className="flex-center w-[60px] h-[37px] bg-[#FF5252] text-white-100 rounded-lg font-bold">
+            D-3
+          </div>
+        </header>
         <section>
           <div className="w-full flex items-center gap-0">
             <button
