a359874d21cf6b86df5af0c4b77060a58d6083ef
yxxndxxn <yxxndxxn@gmail.com>
2025-01-16 15:46:32 +0900

    🔧 fix: 나의 지원 기록 패딩 삭제

--- src/pages/Applicant/ApplicantHistoryDetail.tsx
+++ src/pages/Applicant/ApplicantHistoryDetail.tsx
@@ -7,7 +7,7 @@ export default function ApplicantHistoryDetail() {
     <div className="w-full h-full flex justify-center pt-6 bg-gray-100">
       <Sidemenu />
 
-      <div className="w-[1016px] pl-8 mb-[143px] h-full">
+      <div className="w-[1016px] mb-[143px] h-full">
         <ApplicationHistoryDetailContainer />
       </div>
     </div>
