5a2a126be98dc9e5f69ee47045f9fe578d76527f
yxxndxxn <yxxndxxn@gmail.com>
2025-01-18 04:08:31 +0900

    🔧 fix: 2-2 완료수정 버튼 수정

--- src/components/recruting/_02_prepare/_02/service/Step2.ts
+++ src/components/recruting/_02_prepare/_02/service/Step2.ts
@@ -11,3 +11,15 @@ export async function postPrepare2(data: IdealForm, recruitId: number) {
     throw error;
   }
 }
+
+//PATCH: 완료하기 <-> 수정하기
+export async function patchPrepare2(data: IdealForm, recruitId: number) {
+  try {
+    const response = await Instance.patch(`/plan/stage2/${recruitId}`, data);
+    console.log("모집하기(2) PATCH 성공:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("모집하기(2) PATCH 실패:", error);
+    throw error;
+  }
+}
