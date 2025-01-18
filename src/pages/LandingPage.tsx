60b06c6625d75f98a7ca09e317815d1099a816ed
yeonilil <gwagseoyeon@gmail.com>
2025-01-18 16:23:00 +0900

    ✨ feat: 나머지 섹션 구현

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -254,6 +254,107 @@ export default function LandingPage() {
           className="w-[628px] h-[355px]"
         />
       </section>
+
+      <section className="flex-center gap-4 w-full h-[745px] bg-[#F7F4FF]">
+        <motion.img
+          src="/assets/landingQuestion1.svg"
+          alt="문의 하이라이트1"
+          className="w-[300px]"
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.3 }}
+        />
+        <motion.img
+          src="/assets/landingQuestion2.svg"
+          alt="문의 하이라이트2"
+          className="w-[300px]"
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.3 }}
+        />
+        <motion.img
+          src="/assets/landingQuestion3.svg"
+          alt="문의 하이라이트3"
+          className="w-[300px]"
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.3 }}
+        />
+      </section>
+
+      <section className="relative flex text-left w-full h-[774px] bg-[#575C73] px-[211px]">
+        <div className="text-left pt-[197px] flex flex-col ">
+          <h1 className="text-[45px] font-bold text-white-100">
+            미리 저장한 기본 프로필과 <br />
+            포트폴리오로 편하게 지원하세요!
+          </h1>
+          <p className="font-regular text-[20.89px] text-[#BFC5DC] mt-[30px]">
+            이름, 학교, 학과 등 동아리마다 반복되는 기본 질문들..
+            <br />
+            매번 첨부해야하는 포트폴리오들..
+            <br />
+            번거롭지 않으셨나요!
+            <br />
+            <br />
+            반복되는 질문들과 제출 서류들을
+            <br />
+            한번의 저장으로 끝내세요!
+          </p>
+        </div>
+        <img
+          src="/assets/landingImg2.svg"
+          className="absolute bottom-0 right-[350px] w-[800px]"
+        />
+      </section>
+
+      <div className="flex-center w-full h-[200px] bg-[#EDE9F7] ">
+        <div className=" justify-center flex-col">
+          <p className="flex-center text-[#4D4A61] text-[37px] font-bold">
+            클루팅 이용 과정
+          </p>
+          <p className="text-[#696575] text-[16px]">
+            상세한 활용 방법은 맨 하단의 ‘가이드 자세히 보기’를 확인해 주세요.
+          </p>
+        </div>
+      </div>
+      <div className="flex-center w-full h-[619px] bg-[#13B482]">
+        <img src="/assets/이용과정.svg" className="w-[480px]" />
+        <div className="items-end">
+          <div className="mb-[63px] text-white-100 text-[17px] text-left font-thin">
+            <p>
+              동아리 모집 공고를 구경하고, 지원하고 싶은 동아리의 공고 세부
+              사항을 확인하세요.
+            </p>
+            <p>
+              접수 기간, 합격자 발표일, 활동 상세 내용 등, 지원자가 필요한
+              정보는 다 있어요.
+            </p>
+          </div>
+          <img src="/assets/컴퓨터.svg" className="w-[535px]" />
+        </div>
+      </div>
+      <div className="w-full h-[800px] bg-[#464957] flex flex-col items-center pt-[348px]">
+        <h1 className="text-[45px] font-bold text-white-100">
+          클루팅에 대해 더 궁금하신가요?
+        </h1>
+        <div className="flex gap-4">
+          <Link to="/">
+            <button
+              type="button"
+              className={`w-[210px] h-[54px] rounded-[11px] mt-[50px] button-main-light text-main-100
+           text-body flex-center hover:bg-main-500 hover:text-white-100`}
+            >
+              클루팅 SNS 바로가기
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              type="button"
+              className={`w-[210px] h-[54px] rounded-[11px] mt-[50px] bg-main-100 text-white-100
+         text-body flex-center hover:bg-main-500`}
+            >
+              가이드 자세히 보기
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
